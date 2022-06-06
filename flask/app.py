62deb77a7f2270c2549f39c46266e91ee504a2d1
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2022-06-06 19:17:03 +0000

    speriamo sia l'ultimo

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,4 @@
+#importiamo le librerie che utilizzeremo
 from flask import Flask , request, jsonify, Response
 from flask_restful import Resource , Api ,reqparse
 from flask_cors import CORS
@@ -6,23 +7,23 @@ import pandas as pd
 from flask_pymongo import PyMongo
 import geopandas
 
-app = Flask(__name__ )
+app = Flask(__name__ )#inizializziamo l'app
 
 app.config["MONGO_URI"] = "mongodb://salvini:Forzainter2010@cluster0-shard-00-00.biumq.mongodb.net:27017,cluster0-shard-00-01.biumq.mongodb.net:27017,cluster0-shard-00-02.biumq.mongodb.net:27017/progettoMondiali?ssl=true&replicaSet=atlas-fj2ixn-shard-0&authSource=admin&retryWrites=true&w=majority" #bisogna anche specificare il nome del database al interno del link 
 
-mongo = PyMongo(app)
+mongo = PyMongo(app)#inizializziamo il database
 
-CORS(app)
+CORS(app)#angular riceve i dati passati da flask
 api = Api(app)
 
 
-#-------------------------------------------------------------------------------------------------------------------Prova
+
 @app.route('/')
 def index():
     return ""
 
 class MondialiApi(Resource):
-    def get(self,numAnno):
+    def get(self,numAnno):#viene inviata una richiesta get da angular
         mond = mongo.db.infoMondiali.find() #mondiali dal database
         dfInfo=pd.DataFrame(list(mond)) #mondiali e lo transformo in un dataframe
         world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres')) #recuperiamo geodataframe
@@ -30,27 +31,27 @@ class MondialiApi(Resource):
         anno = dfMerCoo[dfMerCoo["Year"] == numAnno].drop("_id" , axis = 1) #trova lo stato di quel anno, e drop della colonna _id
         return Response(anno.to_json(), mimetype = 'application/json')  #restituisce il json 
 
-api.add_resource(MondialiApi, '/mondiali/<int:numAnno>', endpoint = 'task')
+api.add_resource(MondialiApi, '/mondiali/<int:numAnno>', endpoint = 'task')#endpoint aggiunge nella porta 5000 
 
 
-class CapocannonieriAPI(Resource):
-    def get(self,year):
-        result = mond = mongo.db.capocannonieri.find({"Year" : year},{"_id" : 0}) 
-        resp = json_util.dumps(result)
-        return Response(resp, mimetype = 'application/json') 
+class CapocannonieriAPI(Resource):#
+    def get(self,year):#self proprietà (si mette sempre nelle funzioni di python) e seconda proprietà year
+        result = mond = mongo.db.capocannonieri.find({"Year" : year},{"_id" : 0}) #result=alla find degli anni della collection
+        resp = json_util.dumps(result)#trasforma in json da API
+        return Response(resp, mimetype = 'application/json') #restituisce il json 
 
-api.add_resource(CapocannonieriAPI, '/capocannonieri/<int:year>')
+api.add_resource(CapocannonieriAPI, '/capocannonieri/<int:year>')#endpoint aggiunge nella porta 5000 più l'anno(visualizzo i valori nella porta 5000)
 
-@app.route('/year/<int:year>', methods=['GET'])
+@app.route('/year/<int:year>', methods=['GET'])#routing (se aggiungo "/year/nAnno") mi restituice il valore
 def onedataaa(year):
-    # GET a specific data by nil
+    
     if request.method == 'GET':
         data = mongo.db.stadi.find({'Year': year},{"_id" : 0,"Link":0})
-        resp = json_util.dumps(data)
-        return Response(resp, mimetype = 'application/json') 
+        resp = json_util.dumps(data)#trasforma in json da API
+        return Response(resp, mimetype = 'application/json') #restituisce il json 
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':#se c'è qualcosa nell'API app parte
     app.run()
     
